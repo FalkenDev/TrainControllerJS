8c025ee1d23e6a8fca95f5f68eb100714aae3c5e
FalkenDev <falkendev@gmail.com>
2023-09-04 15:29:47 +0200

    updated route function

--- backend/v1/routes/stations.js
+++ backend/v1/routes/stations.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const stationController = require("../controllers/stations");
 
 router.get("/all", (req, res) =>
-  stationController.getAllStations(res, req.body, req.path)
+  stationController.getStations(res, req.body, req.path)
 );
 
 module.exports = router;
