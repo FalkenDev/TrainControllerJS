85c3dc9830bbe2a729a14d1a1dd75e8d55f4c719
JamesTTTT <taylormarkaryd@gmail.com>
2023-09-04 11:33:38 +0200

    Trainlist & Map
    - Added trainlist with information cards
    - Added Map for trains
    - Added inspectbar

--- frontend/src/components/TripList.vue
+++ frontend/src/components/TripList.vue
@@ -1,9 +1,19 @@
 <script setup>
+import TrainCard from "./TrainCard.vue";
 
+defineProps({
+  trains: Array,
+  toggleDetails: Function,
+});
 </script>
 
 <template>
-<div class="h-full w-full">
-  trips
-</div>
+  <div class="h-full w-full p-3 overflow-y-scroll">
+    <TrainCard
+      v-for="train in trains"
+      @click="toggleDetails"
+      :key="train.trainNr"
+      :trainData="train"
+    />
+  </div>
 </template>
